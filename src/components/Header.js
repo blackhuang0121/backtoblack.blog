5dc45e4dd273c842620e1be247357f9103a3d2e6
Howard Huang <blackhuang0121@gmail.com>
2025-07-09 00:44:53 +0800

    Create category page and Add Posts components for filtering v2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 'use client';
 import SidebarMenu from "./SidebarMenu";
+import Link from 'next/link';
 
 export default function Header() {
     return (
@@ -22,18 +23,18 @@ export default function Header() {
                 {/* row2: Nav 按鈕列（下方一行，置中） */}
                 <nav className="flex items-center justify-center gap-8 mt-8">
                     <div className="flex gap-8">
-                        <a href="/category/trips" className="px-4 py-3 rounded-2xl bg-gray-800 hover:bg-gray-700 text-white text-xl md:text-2xl font-extrabold flex items-center gap-2 shadow-xl transition-all duration-200">
+                        <Link href="/category/trips" className="px-4 py-3 rounded-2xl bg-gray-800 hover:bg-gray-700 text-white text-xl md:text-2xl font-extrabold flex items-center gap-2 shadow-xl transition-all duration-200">
                             🧳 旅遊 Trips
-                        </a>
-                        <a href="#photos" className="px-4 py-3 rounded-2xl bg-gray-800 hover:bg-gray-700 text-white text-xl md:text-2xl font-extrabold flex items-center gap-2 shadow-xl transition-all duration-200">
+                        </Link>
+                        <Link href="#photos" className="px-4 py-3 rounded-2xl bg-gray-800 hover:bg-gray-700 text-white text-xl md:text-2xl font-extrabold flex items-center gap-2 shadow-xl transition-all duration-200">
                             📸 攝影 Photos
-                        </a>
-                        <a href="/category/essays" className="px-4 py-3 rounded-2xl bg-gray-800 hover:bg-gray-700 text-white text-xl md:text-2xl font-extrabold flex items-center gap-2 shadow-xl transition-all duration-200">
+                        </Link>
+                        <Link href="/category/essays" className="px-4 py-3 rounded-2xl bg-gray-800 hover:bg-gray-700 text-white text-xl md:text-2xl font-extrabold flex items-center gap-2 shadow-xl transition-all duration-200">
                             🖋️ 隨筆 Essays
-                        </a>
-                        <a href="/about" className="px-4 py-3 rounded-2xl bg-gray-800 hover:bg-gray-700 text-white text-xl md:text-2xl font-extrabold flex items-center gap-2 shadow-xl transition-all duration-200">
+                        </Link>
+                        <Link href="/about" className="px-4 py-3 rounded-2xl bg-gray-800 hover:bg-gray-700 text-white text-xl md:text-2xl font-extrabold flex items-center gap-2 shadow-xl transition-all duration-200">
                             🖋️ 關於 About
-                        </a>
+                        </Link>
                     </div>
                 </nav>
 
