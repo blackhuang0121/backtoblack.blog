4a7928460a97aaf275221629dadaaa9431a17c18
Howard Huang <blackhuang0121@gmail.com>
2025-07-16 13:24:45 +0800

    Adjust ImageLightbox component for clicking effection

--- src/components/ImageLightbox.js
+++ src/components/ImageLightbox.js
@@ -12,15 +12,27 @@ export default function ImageLightbox({ src, alt, ...props }) {
             {/* 圖片本身，點擊時打開 lightbox */}
             <Image
                 src={src}
-                alt={alt}
+                alt={alt} ㄐ
                 onClick={() => setOpen(true)}
-                style={{ cursor: "zoom-in", borderRadius: "8px", maxWidth: "100%" }}
+                style={{
+                    cursor: "zoom-in",
+                    width: "100%",
+                    height: "100%",
+                    borderRadius: "8px",
+                    objectFit: "contain",
+                    objectPosition: "center",
+                    maxHeight: "100vh",
+                    maxWidth: "100vw"
+                }}
             />
             {/* Lightbox，只顯示一張圖 */}
             <Lightbox
                 open={open}
                 close={() => setOpen(false)}
                 slides={[{ src, alt }]}
+                plugins={[Zoom]}
+                closeOnBackdropClick
+                carousel={false}
             // render={{
             // buttonPrev: () => null,
             // buttonNext: () => null
